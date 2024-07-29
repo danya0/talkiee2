0e7e6e40e05608486197e933a899f4f3ec0430b5
Samarkin Danil <samarkin.danil@yandex.ru>
2024-07-29 15:37:38 +0300

    build: tailwind install

--- app.vue
+++ app.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
+  <div class="">
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1 class="text-6xl">Hello world! {{value}}</h1>
+    <button @click="value++">Plus</button>
+    <button @click="value--">Minus</button>
   </div>
 </template>
+
+<script setup lang="ts">
+const value = useState(() => 12)
+</script>
