9e4629b9ab631f0913700d6ab40e318202e2b20b
Samarkin Danil <samarkin.danil@yandex.ru>
2024-08-01 19:03:55 +0300

    feat: white header if page start to scroll

--- components/logo.vue
+++ components/logo.vue
@@ -1,6 +1,5 @@
 <template>
   <router-link to="/" class="flex items-center gap-x-3">
     <h1 class="logo-title text-[2rem] text-[#4a6fb5]">Talkiee</h1>
-    <div class="border-2 border-[#50688c] rounded-md px-2 py-1">v2.0</div>
   </router-link>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="pb-8 z-30">
     <MyHeader
-      class="z-40 fixed left-1/2 -translate-x-1/2 flex justify-start py-2"
+      ref="headerEl"
+      class="transition-colors z-50 fixed left-1/2 -translate-x-1/2 flex justify-start py-2"
     />
     <Navbar />
     <slot />
@@ -10,4 +11,18 @@
 
 <script setup lang="ts">
 import MyHeader from '~/components/header/myHeader.vue'
+
+const headerEl = ref()
+
+onMounted(() => {
+  window.addEventListener('scroll', (e) => {
+    const el = headerEl.value.$el
+    if (!el) return
+    if (window.scrollY > 30) {
+      el.classList.add('bg-white')
+    } else {
+      el.classList.remove('bg-white')
+    }
+  })
+})
 </script>
