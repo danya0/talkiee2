32b52ede4a6dfcb669a5eb7baab808de4b516b1b
Samarkin Danil <samarkin.danil@yandex.ru>
2024-08-01 20:46:31 +0300

    feat: display a message if favorites are empty

--- pages/favorite/index.vue
+++ pages/favorite/index.vue
@@ -1,6 +1,12 @@
 <template>
   <Container class="pt-24">
-    <MovieGrid :movie-list="favoriteList" />
+    <MovieGrid v-if="favoriteList.length" :movie-list="favoriteList" />
+    <div class="text-center" v-else>
+      <p class="text-3xl">Кажется в избранном пусто 😔</p>
+      <router-link to="/" class="text-lg underline text-blue-400">
+        Вернуться на главную
+      </router-link>
+    </div>
   </Container>
 </template>
 
